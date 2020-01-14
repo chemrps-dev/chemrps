43577bd6a449b0b390171d0a4aaa996e16f6c013
chemrps-dev <59693729+chemrps-dev@users.noreply.github.com>
2020-01-14 15:10:24 +0100

    14.January.2020 3pm

--- chemrpsbuild.bat
+++ chemrpsbuild.bat
@@ -1,22 +1,9 @@
-#call chemrpsenvprepare.bat
 
-#source .env
-
-#./chemrpsenvprepare
-
-#source .env
-
-
-
-#cd ./chemrpsapi
-
-#copy chemrps.env .env
 
 copy .env chemrpsapi /Y
 
-copy .env chemrpsbulksdfileloadconsole /Y
 
-#copy .env chemrpslibrarytestconsole /Y
+
 
 copy .env chemrpsweb /Y
 
@@ -26,7 +13,7 @@ copy .env php /Y
 
 
 
-#copy chemrps.env+.env .env
+
 
 
 docker-compose -f ./chemrps.yml build
